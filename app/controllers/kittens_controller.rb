eedf3a472bb4b5d5cd2e90ef496c049152d0b5df
kenyounot123 <kenlu519@gmail.com>
2023-11-19 22:32:38 -0500

    Add views for Kitten

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,45 @@
+class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end 
+
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  def kitten_params
+  end
+end
