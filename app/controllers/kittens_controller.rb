afd28f0a53b82e9891fd31cebb3ca6541576748c
kenyounot123 <kenlu519@gmail.com>
2023-11-20 19:23:24 -0500

    Update Views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -7,6 +7,7 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
+      flash[:notice] = "New kitten created"
       redirect_to @kitten
     else
       render :new, status: :unprocessable_entity
@@ -26,6 +27,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash[:alert] = "You have successfully changed your kitten"
       redirect_to @kitten
     else
       render :edit, status: :unprocessable_entity
@@ -38,13 +40,12 @@ class KittensController < ApplicationController
   end
 
   def destroy
-    @kitten = Kitten.find(paramsp[:id])
+    @kitten = Kitten.find(params[:id])
     @kitten.destroy
     redirect_to kittens_path, notice: 'Kitten successfully deleted'
-
-
   end
 
   def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
